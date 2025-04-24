afc9d64d737cb95e78bf6b9f9169e3f577614048
Jonathan Sugijono <79694331+ijonathans@users.noreply.github.com>
2025-04-24 09:08:37 -0400

    fix vercel error regarding code issues

--- src/utils/supabase/server.ts
+++ src/utils/supabase/server.ts
@@ -1,5 +1,4 @@
 import { createClient as createSupabaseClient } from '@supabase/supabase-js';
-import { cookies } from 'next/headers';
 
 export function createClient() {
   const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || 'https://jqhojaznhxopvbnkigzg.supabase.co';
